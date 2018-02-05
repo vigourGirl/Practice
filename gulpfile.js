bc5d57ad5af8986f5c4c84e7d518d158b8fbde95
hanyuan <hanyuan@joymaker.cc>
2018-02-05 23:02:28 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -46,9 +46,9 @@ var changed = require('gulp-changed'); // 过滤改变过的文件
 var debug = require('gulp-debug');
 
 // dist
-var dist = '';
+var dist = './';
 // dev
-var dev = '';
+var dev = './';
 
 function errorHandler(e) {
   // 控制台发声,错误时beep一下
@@ -65,9 +65,9 @@ gulp.task('sass', function () {
     }),
     cssnext,
     vmin,
-    cssnano({
-      zindex: false
-    })
+    // cssnano({
+    //   zindex: false
+    // })
   ];
   return gulp.src([
       dev + '/scss/*.scss',
@@ -109,7 +109,7 @@ gulp.task('browser-sync', function () {
     server: './',
     ghostMode: false // 点击，滚动和表单在任何设备上输入将被镜像到所有设备里
   });
-  browserSync.watch(dist + '/**/*.html').on('change', reload);
+  browserSync.watch(dist + '/*.html').on('change', reload);
 });
 
 // js
@@ -180,45 +180,7 @@ gulp.task('tiny', function () {
       stream: true
     }));
 });
-// html
-gulp.task('html', function () {
-  return gulp.src([
-      dev + '/**/*.html',
-      '!' + dev + '/include/*.*',
-      '!' + dev + '/plugins/**/*.*'
-    ])
-    // .pipe(changed(dist))
-    .pipe(plumber({
-      errorHandler: errorHandler
-    }))
-    .pipe(fileinclude({ // 合并html
-      prefix: '@@',
-      basepath: 'dev/include/'
-    }))
-    .pipe(replace('/yunnan/js/vue.js','/yunnan/js/vue.min.js'))
-    .pipe(gulp.dest(dist))
-    .pipe(reload({
-      stream: true
-    }));
-});
-// include
-gulp.task('include', function () {
-  return gulp.src([dev + '/**/*.htm',
-      '!' + dev + '/plugins/**/*.*'
-    ])
-    .pipe(plumber({
-      errorHandler: errorHandler
-    }))
-    .pipe(fileinclude({ // 合并html
-      prefix: '@@',
-      basepath: 'dev/include/'
-    }))
-    // .pipe(debug)
-    .pipe(gulp.dest(dist))
-    .pipe(reload({
-      stream: true
-    }));
-});
+
 gulp.task('plugins', function () {
   return gulp.src([
       dev + '/plugins/**/*.*',
@@ -228,7 +190,7 @@ gulp.task('plugins', function () {
 });
 // clean
 gulp.task('clean', function () {
-  return gulp.src(dist)
+  return gulp.src([dist+'/css/'])
     .pipe(plumber({
       errorHandler: errorHandler
     }))
@@ -239,15 +201,15 @@ gulp.task('clean', function () {
 });
 // watch
 gulp.task('watch', function () {
-  gulp.watch(dev + '/**/*.scss', ['sass']);
-  gulp.watch(dev + '/**/**/*.js', ['js']);
-  gulp.watch(dev + '/**/*.{png,jpg,jpeg,gif,ico,svg}', ['image']);
+  gulp.watch(dev + '/scss/*.scss', ['sass']);
+  // gulp.watch(dev + '/js/**/*.js', ['js']);
+  // gulp.watch(dev + '/img/*.{png,jpg,jpeg,gif,ico,svg}', ['image']);
   // gulp.watch(dev + '/**/*.{png,jpg,jpeg,gif,ico,svg}', ['tiny']);
   // gulp.watch(dev + '/**/*.html', ['html']);
   // gulp.watch(dev + '/include/*.htm', ['include', 'html']);
-  gulp.watch(dev + '/plugins/**/*.*', ['plugins']);
+  // gulp.watch(dev + '/plugins/**/*.*', ['plugins']);
 });
 // 默认任务 清空图片、样式、js并重建 运行语句 gulp
 gulp.task('default', ['clean'], function () {
-  gulp.start('sass', 'js', 'image', 'watch', 'html', 'plugins', 'browser-sync');
+  gulp.start('sass','browser-sync' ,'watch' );
 });
